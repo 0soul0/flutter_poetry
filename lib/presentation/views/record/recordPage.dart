784050b781de29f60215e7488af2ffc18957f9b4
Shu ya <sevideo6666@gmail.com>
2022-11-02 18:54:14 +0800

    bottom

---
+++ lib/presentation/views/record/recordPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RecordPage extends StatelessWidget {
+  const RecordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Record"),
+      ),
+    );
+  }
+}
